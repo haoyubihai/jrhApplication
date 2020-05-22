27328b2ff1b1dfd6ac54bba3b82fd640026a770d
jiaruihua <jiaruihua@ksjgs.com>
2020-05-22 11:23:15 +0800

    wall 打包

--- app/src/main/java/com/example/jrhapplication/ui/FastjsonActivity.kt
+++ app/src/main/java/com/example/jrhapplication/ui/FastjsonActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import com.alibaba.fastjson.JSON
 import com.example.jrhapplication.R
+import groovy.json.JsonOutput
 
 class FastjsonActivity : AppCompatActivity() {
 
@@ -13,22 +14,25 @@ class FastjsonActivity : AppCompatActivity() {
         setContentView(R.layout.activity_fastjson)
 
         val datas = mutableListOf<User>()
-        repeat(5){
-            datas.add(User("name$it",it))
+        repeat(5) {
+            datas.add(User("name$it", it))
         }
 
-        Log.e("FastJson =",JSON.toJSONString(datas))
+        Log.e("FastJson =", JSON.toJSONString(datas))
 
         val str = """
             [{"age":0,"name":"name0"},{"age":1,"name":"name1"},{"age":2,"name":"name2"},{"age":3,"name":"name3"},{"age":4,"name":"name4"}]
         """.trimIndent()
 
-        val users = JSON.parse(str) as MutableList<User>
+//        val users = JSON.parse(str) as MutableList<User>
 
-        users.forEach {
-            Log.e("FastJson USER=",it.toString())
-        }
+//        users.forEach {
+//            Log.e("FastJson USER=",it.toString())
+//        }
+
+        Log.e("JSON----", JsonOutput.prettyPrint(str))
 
     }
 }
-data class User(val name:String,val age:Int)
+
+data class User(val name: String, val age: Int)
---
+++ app/src/main/java/com/example/jrhapplication/utils/JsonOutput.kt
@@ -0,0 +1,28 @@
+package com.example.jrhapplication.utils
+
+/**
+ ***************************************
+ * 项目名称:jrhApplication
+ * @Author jiaruihua
+ * 邮箱：jiaruihua@ksjgs.com
+ * 创建时间: 2020/5/6     2:01 PM
+ * 用途:
+ ***************************************
+ */
+class JsonOutput {
+    companion object {
+        val OPEN_BRACKET :Char= '['
+        val CLOSE_BRACKET:Char = ']'
+        val OPEN_BRACE :Char= '{'
+        val CLOSE_BRACE :Char= '}'
+        val COLON :Char= ':'
+        val COMMA:Char = ','
+        val SPACE :Char= ' '
+        val NEW_LINE :Char= '\n'
+        val QUOTE :Char= '"'
+        val EMPTY_STRING_CHARS = charArrayOf('"', '"')
+        val EMPTY_MAP_CHARS = charArrayOf('{', '}')
+        val EMPTY_LIST_CHARS = charArrayOf('[', ']')
+    }
+
+}
